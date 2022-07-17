9a17b19cbbccf40076c81c4c8247482cfb78d22e
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-07-17 23:04:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,31 +1,17 @@
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `
-┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
-│ • *Telkomsel:* [${global.ppulsa2}]
-❏────
-
-┌─「 Donasi • Non Pulsa 」
-│ • *Dana:* [${global.pdana}]
-❏────
-`
-const templateButtons = [
-    {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
-    {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
-    {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
-    {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
-    {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
-]
-let tm = {
-text: text,
-footer: global.wm,
-templateButtons: templateButtons,
-image: {url: fla + 'Donasi'}
-}
-conn.sendMessage(m.chat, tm, m)
-}
+let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
+┏──「 Donasi • Dana 」─⬣
+│ • Gopay : 088233832771
+│ • Dana : 088233832771
+╰────────⬣
+┏──「 *NOTE* 」─⬣
+│ > Ingin donasi? wa.me/6288233832771
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24jam tanpa kendala_
+┗────────⬣
+`.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
-handler.tags = ['info']
+handler.tags = ['about']
 handler.command = /^dona(te|si)$/i
 
-export default handler
+module.exports = handler
